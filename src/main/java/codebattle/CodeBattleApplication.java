704f13a433e26ae87738fd077a73395213bfeab4
leandro.sebold <leandro.sebold@edu.sc.senai.br>
2023-02-08 19:17:02 -0300

    Inicia projeto

---
+++ src/main/java/codebattle/CodeBattleApplication.java
@@ -0,0 +1,13 @@
+package codebattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codebattle/CodeBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package codebattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
